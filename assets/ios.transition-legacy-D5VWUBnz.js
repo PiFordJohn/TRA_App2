bdc23700e0576e41e840ea74786518221cd558c5
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2025-05-25 23:57:14 +0800

    Updates

--- assets/ios.transition-legacy-D5VWUBnz.js
+++ assets/ios.transition-legacy-D5VWUBnz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C6dIgqcV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-fqyXC0Gj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
