283e58aad74c9d1862076907973a90c5b8cf27f4
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2025-05-28 19:53:18 +0800

    Updates

--- assets/md.transition-legacy-kp9cqvVM.js
+++ assets/md.transition-legacy-kp9cqvVM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DgBANQzc.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BoGq6Tuu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
